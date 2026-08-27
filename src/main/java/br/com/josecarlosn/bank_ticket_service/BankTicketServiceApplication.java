8d2e03166480ff9a6624eeb8140a096b51635aaa
José <zecarlosnetodantas@gmail.com>
2026-08-27 14:49:18 -0300

    primeiro commit

---
+++ src/main/java/br/com/josecarlosn/bank_ticket_service/BankTicketServiceApplication.java
@@ -0,0 +1,15 @@
+package br.com.josecarlosn.bank_ticket_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankTicketServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankTicketServiceApplication.class, args);
+		System.out.print("Server running");
+	}
+
+
+}
---
+++ src/test/java/br/com/josecarlosn/bank_ticket_service/BankTicketServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.josecarlosn.bank_ticket_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankTicketServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
