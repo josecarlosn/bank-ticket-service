54374090d3da7e74ee33dd0c38bfc9742757ad04
José <zecarlosnetodantas@gmail.com>
2026-09-08 16:50:32 -0300

    update: improve DTO request validations

--- src/main/java/br/com/josecarlosn/bank_ticket_service/dto/request/DepartmentRequestDTO.java
+++ src/main/java/br/com/josecarlosn/bank_ticket_service/dto/request/DepartmentRequestDTO.java
@@ -2,13 +2,15 @@ package br.com.josecarlosn.bank_ticket_service.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record DepartmentRequestDTO(
-        @NotNull @NotBlank(message = "Name field can't be empty!")
+
+        @NotNull @Size(max = 100, message = "Name field can't have more than 100 characters!") @NotBlank(message = "Name field can't be empty!")
         String name,
-        @NotNull @NotBlank(message = "Tag field can't be empty!")
+        @NotNull @Size(max = 5, message = "Tag field can't have more than 5 characters!") @NotBlank(message = "Tag field can't be empty!")
         String tag,
-        @NotNull @NotBlank(message = "Priority tag field can't be empty!")
+        @NotNull @Size(max = 5, message = "Priority tag field can't have more than 5 characters!") @NotBlank(message = "Priority tag field can't be empty!")
         String priorityTag
 ) {
 }
